8d0ec407ea85a47f126b0b4424871d8d496bd9ae
CPALyth <ws156858@163.com>
2021-05-09 17:56:06 +0800

    整理项目结构

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -16,7 +16,11 @@ SERVER_IP = '192.168.250.130'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
+import sys
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+print(sys.path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -159,7 +164,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # 配置静态文件的加载路径
-STATIC_FILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
