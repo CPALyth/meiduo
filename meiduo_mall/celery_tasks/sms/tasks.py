e5fd8d88b7e52e95b0d3c9783f1c44197d623a63
CPALyth <ws156858@163.com>
2021-05-12 07:21:57 +0800

    celery错误重试机制

--- meiduo_mall/celery_tasks/sms/tasks.py
+++ meiduo_mall/celery_tasks/sms/tasks.py
@@ -1,9 +1,17 @@
+import logging
+
 from .yuntongxun.ccp_sms import CCP
 from verifications import constants
 from celery_tasks.main import celery_app
 
-@celery_app.task(name='send_sms_code')
-def send_sms_code(mobile, sms_code):
-    send_ret = CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60], constants.SEND_SMS_TEMPLATE_ID)
+logger = logging.getLogger('django')
+
+@celery_app.task(bind=True, name='send_sms_code', retry_backoff=5)
+def send_sms_code(self, mobile, sms_code):
+    try:
+        send_ret = CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60], constants.SEND_SMS_TEMPLATE_ID)
+    except Exception as e:
+        logger.error(e)
+        raise self.retry(exc=e, max_retries=3)
     return send_ret
 
