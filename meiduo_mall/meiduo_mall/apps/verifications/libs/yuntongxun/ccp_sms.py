32f32da6a08615453eb02056be7e417b26600683
CPALyth <ws156858@163.com>
2021-05-11 09:06:54 +0800

    短信验证码

---
+++ meiduo_mall/meiduo_mall/apps/verifications/libs/yuntongxun/ccp_sms.py
@@ -0,0 +1,57 @@
+# -*- coding:utf-8 -*-
+
+import ssl
+ssl._create_default_https_context =ssl._create_stdlib_context # 解决Mac开发环境下，网络错误的问题
+
+from verifications.libs.yuntongxun.CCPRestSDK import REST
+
+# 说明：主账号，登陆云通讯网站后，可在"控制台-应用"中看到开发者主账号ACCOUNT SID
+_accountSid = '8a216da878d425020178ea9e6d1d082e'
+
+# 说明：主账号Token，登陆云通讯网站后，可在控制台-应用中看到开发者主账号AUTH TOKEN
+_accountToken = 'ca1592d37d1d49a888267efa3513e75c'
+
+# 请使用管理控制台首页的APPID或自己创建应用的APPID
+_appId = '8a216da878d425020178ea9e6e2c0835'
+
+# 说明：请求地址，生产环境配置成app.cloopen.com
+_serverIP = 'app.cloopen.com'
+
+# 说明：请求端口 ，生产环境为8883
+_serverPort = "8883"
+
+# 说明：REST API版本号保持不变
+_softVersion = '2013-12-26'
+
+class CCP():
+    """发送短信验证码的单例类"""
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            # 如果单例不存在, 初始化单例
+            cls._instance = super().__new__(cls, *args, **kwargs)
+            # 初始化REST SDK
+            cls._instance.rest = REST(_serverIP, _serverPort, _softVersion)
+            cls._instance.rest.setAccount(_accountSid, _accountToken)
+            cls._instance.rest.setAppId(_appId)
+        return cls._instance
+
+    def send_template_sms(self, to, datas, tempId):
+        """
+        发送短信验证码单例方法
+        :param to: 手机号
+        :param datas: 内容数据
+        :param tempId: 模板ID
+        :return: 成功0, 失败-1
+        """
+        result = self.rest.sendTemplateSMS(to, datas, tempId)
+        print(result)
+        if result.get('statusCode') == '000000':
+            return 0
+        else:
+            return -1
+
+
+if __name__ == '__main__':
+    # 注意： 测试的短信模板编号为1
+    ret = CCP().send_template_sms('15622746858', ['123456', 5], 1)
+    print(ret)
--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,9 +1,55 @@
-from django.shortcuts import render
+import logging, random
+
 from django.views import View
 from django_redis import get_redis_connection
 from django import http
 
-from verifications.libs.captcha.captcha import captcha
+from meiduo_mall.utils.response_code import RETCODE
+from .libs.captcha.captcha import captcha
+from .libs import constants
+
+# 创建日志输出器
+from .libs.yuntongxun.ccp_sms import CCP
+
+logger = logging.getLogger('django')
+
+class SMSCodeView(View):
+    """短信验证码"""
+    def get(self, request, mobile):
+        """
+        :param request: 请求对象
+        :param mobile: 手机号
+        :return: JSON
+        """
+        # 接收参数
+        image_code_client = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
+        # 校验参数
+        if not all([mobile, image_code_client, uuid]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 提取图形验证码
+        redis_conn = get_redis_connection('verify_code')
+        redis_key = 'img_{}'.format(uuid)
+        image_code_server = redis_conn.get(redis_key)
+        if image_code_server is None:
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码已失效'})
+        # 删除图形验证码
+        redis_conn.delete(redis_key)
+        # 对比图形验证码
+        image_code_server = image_code_server.decode()
+        if image_code_client.lower() != image_code_server.lower():
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '输入图形验证码有误'})
+        # 生成短信验证码: 随机六位数字
+        sms_code = '{:06d}'.format(random.randint(0, 999999))
+        logger.info(sms_code)
+        # 保存短信验证码
+        redis_conn.setex(redis_key, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        # 发送短信验证码
+        CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60], constants.SEND_SMS_TEMPLATE_ID)
+        # 响应结果
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信成功'})
+
+
 
 
 class ImageCodeView(View):
@@ -19,7 +65,7 @@ class ImageCodeView(View):
 
         # 保存图形验证码
         redis_conn = get_redis_connection('verify_code')
-        redis_conn.setex("img_{}".format(uuid), 300, text)  # key, 过期时间秒, value
+        redis_conn.setex("img_{}".format(uuid), constants.IMAGE_CODE_REDIS_EXPIRES, text)  # key, 过期时间秒, value
 
         # 响应图形验证码, MIME类型为'image/jpg'
         return http.HttpResponse(image, content_type='image/jpg')
