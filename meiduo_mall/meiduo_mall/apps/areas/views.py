a0a924350009e2438b67a5e16d2e1a1cc23bcb95
CPALyth <ws156858@163.com>
2021-05-12 09:41:26 +0800

    缓存省市区数据

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -3,6 +3,7 @@ import logging
 from django.shortcuts import render
 from django.views import View
 from django import http
+from django.core.cache import cache
 
 from .models import Area
 from meiduo_mall.utils.response_code import RETCODE
@@ -14,34 +15,44 @@ class AreasView(View):
     def get(self, request):
         area_id = request.GET.get('area_id')
         if not area_id:  # 查询省级数据
-            try:
-                province_list = []
-                province_model_list = Area.objects.filter(parent_id__isnull=True)
-                for province in province_model_list:
-                    province_list.append({
-                        'id': province.id,
-                        'name': province.name,
-                    })
-                return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'province_list': province_list})
-            except Exception as e:
-                logger.error(e)
-                return http.JsonResponse({'code': RETCODE.DBERR, 'errmsg': '查询省份数据错误'})
+            # 查询是否有缓存
+            province_list = cache.get('province_list')
+            if not province_list:
+                try:
+                    province_list = []
+                    province_model_list = Area.objects.filter(parent_id__isnull=True)
+                    for province in province_model_list:
+                        province_list.append({
+                            'id': province.id,
+                            'name': province.name,
+                        })
+                    # 缓存省份列表, 默认存放到'default'CACHES中, 即0号redis数据库
+                    cache.set('province_list', province_list, 3600)
+                except Exception as e:
+                    logger.error(e)
+                    return http.JsonResponse({'code': RETCODE.DBERR, 'errmsg': '查询省份数据错误'})
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'province_list': province_list})
         else:  # 查询城市或区县数据
-            try:
-                parent_model = Area.objects.get(id=area_id)
-                sub_model_list = parent_model.subs.all()
-                subs = []
-                for sub_model in sub_model_list:
-                    subs.append({
-                        'id': sub_model.id,
-                        'name': sub_model.name,
-                    })
-                sub_data = {
-                    'id': parent_model.id,
-                    'name': parent_model.name,
-                    'subs': subs,
-                }
-                return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'sub_data': sub_data})
-            except Exception as e:
-                logger.error(e)
-                return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '查询城市或区域数据错误'})
+            # 查询是否有缓存
+            sub_data = cache.get('sub_area_' + area_id)
+            if not sub_data:
+                try:
+                    parent_model = Area.objects.get(id=area_id)
+                    sub_model_list = parent_model.subs.all()
+                    subs = []
+                    for sub_model in sub_model_list:
+                        subs.append({
+                            'id': sub_model.id,
+                            'name': sub_model.name,
+                        })
+                    sub_data = {
+                        'id': parent_model.id,
+                        'name': parent_model.name,
+                        'subs': subs,
+                    }
+                    # 缓存城市或区县
+                    cache.set('sub_area_' + area_id, sub_data, 3600)
+                except Exception as e:
+                    logger.error(e)
+                    return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '查询城市或区域数据错误'})
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'sub_data': sub_data})
