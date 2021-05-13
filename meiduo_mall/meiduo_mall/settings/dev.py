207f03bb68610557f37cf49c5ff75e015293f5e9
CPALyth <ws156858@163.com>
2021-05-13 16:12:29 +0800

    商品搜索

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'oauth',
     'areas',
     'goods',
+    'haystack',  # 全文检索
 ]
 
 MIDDLEWARE = [
@@ -254,3 +255,16 @@ DEFAULT_FILE_STORAGE = 'meiduo_mall.utils.fastdfs.fdfs_storage.FastDFSStorage'
 
 # FastDFS相关参数
 FDFS_BASE_URL = 'http://image.meiduo.site:8888/'
+
+# Haystack
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://{}:9200/'.format(SERVER_IP), # Elasticsearch服务器ip地址，端口号固定为9200
+        'INDEX_NAME': 'meiduo_mall', # Elasticsearch建立的索引库的名称
+    },
+}
+# 当添加、修改、删除数据时，自动生成索引
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+# 每页显示五条数据
+HAYSTACK_SEARCH_RESULTS_PER_PAGE = 5
--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', include('haystack.urls')),
     path('', include('users.urls')),
     path('', include('contents.urls')),
     path('', include('verifications.urls')),
