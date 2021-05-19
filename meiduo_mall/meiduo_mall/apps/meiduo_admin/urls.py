e7c61f38aa95011715c4f47e9e86424d031b86f2
CPALyth <ws156858@163.com>
2021-05-19 10:59:57 +0800

    今日下单用户数统计

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
     # 后台登录
     path('authorizations/', obtain_jwt_token),
     # ---------------- 数据统计 ----------------
+    # 用户总数
     path('statistical/total_count/', statistical.UserTotalCountView.as_view()),
+    # 日增用户
+    path('statistical/day_increment/', statistical.UserDayIncrementView.as_view()),
+    # 日活用户
+    path('statistical/day_active/', statistical.UserDayActiveView.as_view()),
+    # 下单用户
+    path('statistical/day_orders/', statistical.UserDayOrderView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/statistical.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/statistical.py
@@ -19,3 +19,46 @@ class UserTotalCountView(APIView):
             'count': count,
             'date': now_date,
         })
+
+class UserDayIncrementView(APIView):
+    """日增用户统计"""
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        # 获取当前日期
+        now_date = datetime.date.today()
+        # 获取当日注册用户数量 date_joined 记录创建账户时间
+        count = User.objects.filter(date_joined__gte=now_date).count()
+        return Response({
+            'count': count,
+            'date': now_date,
+        })
+
+class UserDayActiveView(APIView):
+    """日活用户统计"""
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        # 获取当前日期
+        now_date = datetime.date.today()
+        # 获取日活用户个数
+        count = User.objects.filter(last_login__gte=now_date).count()
+        return Response({
+            'count': count,
+            'date': now_date,
+        })
+
+
+class UserDayOrderView(APIView):
+    """今日下单用户数统计"""
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        # 获取当前日期
+        now_date = datetime.date.today()
+        # 获取今日下单用户数, 通过订单条件查询用户, 关联查询多查一
+        count = User.objects.filter(orderinfo__create_time__gte=now_date).distinct().count()
+        return Response({
+            'count': count,
+            'date': now_date,
+        })
