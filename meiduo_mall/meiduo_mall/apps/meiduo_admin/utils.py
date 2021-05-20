0b3964cb29f3913a3d5eca2ff44b6cd80ad1a5f1
CPALyth <ws156858@163.com>
2021-05-20 15:09:44 +0800

    改写分页器返回结果

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/utils.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/utils.py
@@ -1,6 +1,29 @@
+from collections import OrderedDict
+
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
 def jwt_response_payload_handler(token, user=None, request=None):
     return {
         'token': token,
         'username': user.username,
         'id': user.id,
     }
+
+class MyPagination(PageNumberPagination):
+    """自定义分页器"""
+    page_size = 3  # 每页数量
+    max_page_size = 4  # 前端最多能设置的每页数量
+    page_query_param = 'page'  # page_query_param 前端发送的页数关键字名，默认为"page"
+    page_size_query_param = 'pagesize'  # 前端发送的每页数目关键字名，默认为None
+
+    def get_paginated_response(self, data):
+        """改写分页器返回结果"""
+        return Response(OrderedDict([
+            ('counts', self.page.paginator.count),
+            ('lists', data),
+            ('page', self.page.number),
+            ('pages', self.page.paginator.num_pages),
+            ('pagesize', len(data)),
+        ]))
