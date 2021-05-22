415172cf375e00a350c6a8d77b08f91b7fecdbf3
CPALyth <ws156858@163.com>
2021-05-22 12:01:20 +0800

    上传图片代码封装到序列化器中

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/images.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/images.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from fdfs_client.client import Fdfs_client
 from rest_framework import serializers
 
 from goods.models import SKUImage, SKU
@@ -8,6 +10,23 @@ class ImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = SKUImage
         fields = ['id', 'sku', 'image']
+
+    def create(self, validated_data):
+        # 创建FDFS客户端对象
+        cli = Fdfs_client(settings.FDFS_CLIENT_PATH)
+        # 读取文件
+        request = self.context['request']
+        file = request.FILES.get('image').read()
+        # 上传文件
+        ret = cli.upload_by_buffer(file)
+        # 判断是否上传成功
+        if ret['Status'] != 'Upload successed.':
+            raise serializers.ValidationError({'error': '上传图片失败'})
+        # 在数据库中新增图片
+        sku_id = validated_data.get('sku').id
+        image = ret['Remote file_id']
+        sku_img = SKUImage.objects.create(sku_id=sku_id, image=image)
+        return sku_img
 
 
 class SKUSerializer(serializers.ModelSerializer):
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/images.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/images.py
@@ -24,28 +24,3 @@ class ImagesView(ModelViewSet):
         ser = SKUSerializer(skus, many=True)
         return Response(ser.data)
 
-    def create(self, request: Request, *args, **kwargs):
-        # 获取前端数据
-        data = request.data
-        # 验证数据
-        ser = self.get_serializer(data=data)
-        ser.is_valid()
-        # 创建FDFS客户端对象
-        cli = Fdfs_client(settings.FDFS_CLIENT_PATH)
-        # 读取文件
-        file = request.FILES.get('image').read()
-        # 上传文件
-        ret = cli.upload_by_buffer(file)
-        # 判断是否上传成功
-        if ret['Status'] != 'Upload successed.':
-            return Response({'error': '上传图片失败'})
-        # 在数据库中新增图片
-        sku_id = ser.validated_data.get('sku').id
-        image = ret['Remote file_id']
-        sku_img = SKUImage.objects.create(sku_id=sku_id, image=image)
-        # 返回数据
-        return Response({
-            'id': sku_img.id,
-            'sku': sku_img.sku_id,
-            'image': sku_img.image.url,
-        })
