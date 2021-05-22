44fae10633d0bba703685c2b26a17ec58c2be7e8
CPALyth <ws156858@163.com>
2021-05-22 15:27:51 +0800

    修改商品图片

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/images.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/images.py
@@ -28,6 +28,23 @@ class ImagesSerializer(serializers.ModelSerializer):
         sku_img = SKUImage.objects.create(sku_id=sku_id, image=image)
         return sku_img
 
+    def update(self, instance, validated_data):
+        # 创建FDFS客户端对象
+        cli = Fdfs_client(settings.FDFS_CLIENT_PATH)
+        # 读取文件
+        request = self.context['request']
+        file = request.FILES.get('image').read()
+        # 上传文件
+        ret = cli.upload_by_buffer(file)
+        # 判断是否上传成功
+        if ret['Status'] != 'Upload successed.':
+            raise serializers.ValidationError({'error': '上传图片失败'})
+        # 在数据库中修改图片
+        image = ret['Remote file_id']
+        instance.image = image
+        instance.save()
+        return instance
+
 
 class SKUSerializer(serializers.ModelSerializer):
     """SKU序列化器"""
