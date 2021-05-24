19a92b352fc91a7e08ed6d70bd2475c9c5b65317
CPALyth <ws156858@163.com>
2021-05-24 20:33:16 +0800

    修改订单状态

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/orders.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/orders.py
@@ -1,5 +1,7 @@
+from rest_framework.decorators import action
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
 from orders.models import OrderInfo
 from ..serializers.orders import OrderInfoSerializer, OrderSerializer
@@ -20,3 +22,22 @@ class OrderView(ReadOnlyModelViewSet):
         if keyword:
             return OrderInfo.objects.filter(order_id__contains=keyword)
         return OrderInfo.objects.all()
+
+    @action(methods=['put'], detail=True)
+    def status(self, request, pk):
+        """
+        pk: order_id
+        """
+        # 获取订单
+        try:
+            order = OrderInfo.objects.get(order_id=pk)
+        except:
+            return Response({'error': '订单编号错误'})
+        # 修改状态
+        status = request.data.get('status')
+        if not status:
+            return Response({'error': '缺少状态值'})
+        order.status = status
+        order.save()
+        # 返回修改信息
+        return Response({'order_id': pk, 'status': status})
