187bb2af75121ca669202e92d3cdc34bc3fef8e9
CPALyth <ws156858@163.com>
2021-05-24 15:58:40 +0800

    获取订单表详情

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/orders.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/orders.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
 
-from orders.models import OrderInfo
+from orders.models import OrderInfo, OrderGoods, SKU
+
+
+class SKUSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SKU
+        fields = ('name', 'default_image')
+
+
+class OrderGoodsSerializer(serializers.ModelSerializer):
+    sku = SKUSerializer(read_only=True)
+
+    class Meta:
+        model = OrderGoods
+        fields = ('count', 'price', 'sku')
 
 
 class OrderInfoSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    skus = OrderGoodsSerializer(read_only=True, many=True)
+
     class Meta:
         model = OrderInfo
-        fields = ('order_id', 'create_time')
+        fields = '__all__'  # ('order_id', 'create_time')
