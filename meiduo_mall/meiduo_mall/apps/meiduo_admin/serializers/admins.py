30d9c8c69022b8479da450a4af850ada9502f7cb
CPALyth <ws156858@163.com>
2021-05-26 16:09:31 +0800

    新增/修改管理员信息

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/admins.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/admins.py
@@ -7,3 +7,18 @@ class AdminSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """父类的保存用户不会使之成为管理员, 密码也不会加密存储"""
+        user = super().create(validated_data)
+        user.is_staff = True
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        """父类的更新用户不会加密存储密码"""
+        user = super().update(instance, validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
