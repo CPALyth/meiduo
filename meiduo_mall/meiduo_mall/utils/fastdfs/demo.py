6ddee415568df65ae374b478a4381a569183ff6d
ws <ws156858@163.com>
2021-06-16 16:12:31 +0800

    修复 fdfs上传文件

--- meiduo_mall/meiduo_mall/utils/fastdfs/demo.py
+++ meiduo_mall/meiduo_mall/utils/fastdfs/demo.py
@@ -1,10 +1,16 @@
-from fdfs_client.client import Fdfs_client
+from fdfs_client.client import Fdfs_client, get_tracker_conf
+
+from django.conf import settings
 
 if __name__ == '__main__':
+    import os
+    if not os.getenv('DJANGO_SETTINGS_MODULE'):
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'meiduo_mall.settings.dev'
     # 创建连接对象
-    client = Fdfs_client('client.conf')
+    tracker_path = get_tracker_conf(settings.FDFS_CLIENT_PATH)
+    client = Fdfs_client(tracker_path)
     # 上传文件
-    ret = client.upload_by_filename('/Users/ws/Desktop/1.jpg')
+    ret = client.upload_by_filename('/home/ws/Desktop/1.jpg')
     # 响应值
     print(ret)
 
